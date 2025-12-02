9c26e6f4c234c66b1769e449f25244f07891b8d0
ABema <a.maya3141592@icloud.com>
2025-12-02 18:25:28 +0900

    アストロキャンプ向けのメーリングリストの作成を行った
    Signed-off-by: ABema <a.maya3141592@icloud.com>

--- src/map.ts
+++ src/map.ts
@@ -12,4 +12,12 @@ export const forwardingEmailMap: ForwardingEmailMap = {
 		'suzumejiro8@gmail.com',
 		'kazuki.fujita8823@gmail.com',
 	],
+	'astrocampe@ase-lab.space': [
+		'maya.abe.crescent@gmail.com', //MaAbe
+		'spes.2012skicem@gmail.com', //MeKanda
+		'kazuki.fujita8823@gmail.com', //KaFujita
+		'spring.snowman0625@gmail.com', //YuImaizumi
+		'mt20ut327@gmail.com', //TaMiyazaki
+		'suzumejiro8@gmail.com', //ShWatanabe
+	],
 };
